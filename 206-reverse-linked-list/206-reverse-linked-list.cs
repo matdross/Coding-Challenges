695315a6c85ed43cfbdf86a0694b0192f41f64db
Mathew Ross <31719422+matdross@users.noreply.github.com>
2022-08-17 19:44:41 +1000

    Time: 112 ms (67.90%), Space: 37.7 MB (82.18%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        //Flip the pointer of next from n+1 to n. Hold n+1 pointer
+        //Init prev, cur, next
+        //next to next, current next = prev (null to start)
+        //prev to current
+        //iterate to next
+        ListNode prev = null, current = head, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;   
+        return head;
+    }
+}

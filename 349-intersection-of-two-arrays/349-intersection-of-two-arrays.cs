182c75d1478fbfe38a0696d85ade99ff26cc0918
Mathew Ross <31719422+matdross@users.noreply.github.com>
2022-08-27 16:40:39 +1000

    Time: 223 ms (40.54%), Space: 41.7 MB (61.64%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cs
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cs
@@ -19,24 +19,3 @@ public class Solution {
         return result;
     }
 }
-
-/*
-HashSet<Integer> set = new HashSet<Integer>();
-for(int i: nums1){
-    set.add(i);
-}
-
-HashSet<Integer> intersection = new HashSet<Integer>();
-for(int i: nums2){
-    if(set.contains(i)){
-        intersection.add(i);
-    }
-}
-int[] result = new int[intersection.size()];
-int index = 0;
-for(int i: intersection){
-    result[index++] = i;
-}
-
-return result;
-*/

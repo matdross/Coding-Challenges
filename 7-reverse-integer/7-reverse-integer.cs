9de6da43567c861255e3a361b12e9d35ab59daf3
Mathew Ross <31719422+matdross@users.noreply.github.com>
2022-09-13 17:04:23 +1000

    Time: 18 ms (99.00%), Space: 25.2 MB (77.88%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Reverse(int x) {
+
+        int ans = 0;
+        while (x != 0)
+        {
+            if (ans > 214748364 || ans < -214748364)
+            {
+                return 0;
+            }
+            
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        }  
+        return ans;
+        
+    }
+}
